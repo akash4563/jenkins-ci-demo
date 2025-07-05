01631fad60734342abdc99f567aca125abc2bdba
akash4563 <das.akash4563@gmail.com>
2025-07-06 00:50:35 +0530

    Updated code for CI test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
+
 public class App {
    public static void main(String[] args) {
-       System.out.println("Hello from Jenkins CI Pipeline!");
+       System.out.println("Hello, this is an updated build triggered by Jenkins CI!");
    }
 }
